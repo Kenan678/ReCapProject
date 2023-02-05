59bbd5e7716fa9d1577e0831e5b65263c251c3b7
Khanan Mammadov <khanan.mammadov@khazar.org>
2023-02-06 03:14:47 +0400

    dto

--- Core/DataAccces/EntityFramework/IEntityRepository.cs
+++ Core/DataAccces/EntityFramework/IEntityRepository.cs
@@ -10,8 +10,8 @@ namespace Core.DataAccess.EntityFramework
         T Get(Expression<Func<T, bool>> filter);
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
+        void Update(T entity)
+        void Delete(T entity); 
 
     }
 }
