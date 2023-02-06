286275e130e8fda1dbbea5cb7a800aee76b12c70
Khanan Mammadov <khanan.mammadov@khazar.org>
2023-02-07 01:16:40 +0400

    DTOS

---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto:IDto
+    {
+        public int CarName { get; set; }
+        public int CategoryId { get; set; }
+        public int Cate { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
